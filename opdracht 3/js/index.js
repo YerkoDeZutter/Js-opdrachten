0dcd22778397ce42181d3f4ffd6f2655f2b3937e
Yerko <yerkodezutter@gmail.com>
2019-01-29 09:30:20 +0100

    oprdacht 2 van 3 vlaar

--- opdracht 3/js/index.js
+++ opdracht 3/js/index.js
@@ -1,4 +1,16 @@
 
-function op1s1(){
-  hoverImg.style.backgroundImage = "url('img/Rintaro-2.png')"
+// ----- OPDRACHT 1 -----
+
+// function op1s1(){
+//   hoverImg.style.backgroundImage = "url('img/Rintaro-2.png')"
+// }
+
+
+
+
+
+// ----- OPDRACHT 2 -----
+
+function op2s1(){
+  alert("Dank u voor uw deelname")
 }
